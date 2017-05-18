7111a6ff9e3ab55106e54627fe6308affab5359f
dye784 <damon.ye91@gmail.com>
2017-05-18 18:22:29 -0400

    feat: integrate pix api, display cats on query

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Image, ListView } from 'react-native';
+import { PIXABAY_API_KEY } from './.secrets.env'
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.webformatURL !== r2.webformatURL });
+    this.state = {
+      searchFor: '',
+      dataSource: ds.cloneWithRows([]),
+      selectedItem: {},
+    };
+  }
+
+  handleSearchQuery = () => {
+    const { dataSource, searchFor } = this.state
+    fetch(`https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${searchFor}&image_type=photo`)
+      .then((res) => res.json())
+      .then((resJson) => resJson.hits)
+      .then((foundImages) => {
+        const newDataSource = dataSource.cloneWithRows(foundImages);
+        this.setState({ searchFor: '', dataSource: newDataSource })
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
+    const { dataSource, searchFor } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Search For Something!</Text>
+        <Text>Recommendation: Cats!</Text>
+        <TextInput
+          placeholder="Enter something to search for!"
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(searchFor) => this.setState({ searchFor })}
+          onEndEditing={this.handleSearchQuery}
+          value={searchFor}
+        />
+        <ListView
+          enableEmptySections={true}
+          dataSource={dataSource}
+          renderRow={(rowData) => <Image style={{ width: 200, height: 200 }} source={{ uri: rowData.webformatURL }} />}
+        />
       </View>
     );
   }
@@ -16,6 +53,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
